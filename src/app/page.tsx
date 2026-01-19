ab3d1022f6f2d697a20130ad9ee18e62d1a4839d
upahar windows <upaharlol@gmail.com>
2026-01-19 21:18:39 +0545

    refactor: clean up error handling and unused imports in TRPC routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,41 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { trpc } from "./_trpc/client";
 
 export default function Hello() {
   const [sum, setSum] = useState<number | null>(null);
 
-  const hello = trpc.hello.useQuery();
+  const { data, error } = trpc.hello.useQuery();
 
-  useEffect(() => {
-    alert(hello.error);
-    if (hello.error?.data?.code === "TOO_MANY_REQUESTS") {
-      alert("Rate limit reached! Try again later!");
-    }
-  }, [hello.error]);
+  console.log(error?.message);
+  console.log(data);
+
+  // const hello = trpc.hello.useQuery(undefined, {
+  //   onError: (err) => {
+  //     if (err.data?.code === "TOO_MANY_REQUESTS") {
+  //       alert("Rate limit reached! Try again later!");
+  //     }
+  //   },
+  // });
+
+  // useEffect(() => {
+  //   // alert(hello.error);
+  //   if (hello.error?.data?.code === "TOO_MANY_REQUESTS") {
+  //     alert("Rate limit reached! Try again later!");
+  //   }
+  // }, [hello.error]);
 
   const echo = trpc.echo.useMutation({
     onError: (err) => {
-      if (err.data?.code === "TOO_MANY_REQUESTS") {
-        alert("Rate limit reached! Try again later.");
-      }
+      alert(err.message);
     },
   });
 
   const addMutation = trpc.add.useMutation({
     onSuccess: (data) => setSum(data.result),
     onError: (err) => {
-      if (err.data?.code === "TOO_MANY_REQUESTS") {
-        alert("Rate limit reached! Try again later.");
-      }
+      alert(err.message); // shows TOO_MANY_REQUESTS
     },
   });
 
